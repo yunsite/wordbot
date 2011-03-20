daaec4e75dde1a7c1e40312ad97d3f604f82b0a1
chenxiaoqino <chenxiaoqino@gmail.com>
2011-03-20 09:52:28 +0000

    delete follow back codes,  currently useless

--- fw.php
+++ fw.php
@@ -47,21 +47,6 @@ if($follower->description!='') print '( '.$follower->description.' )';
 else print "--";
 echo '<br>'.$follower->status->text.' <br>'; 
 
-if($follower->followed==false){
-//echo 'not following';
-/*
-$curl3 = curl_init(); 
-curl_setopt($curl3, CURLOPT_URL,"http:// api.t.sina.com.cn/friendships/create/".$follower->name.".xml?source=".SWBAPPKEY);     
-//curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);     
-//curl_setopt($curl,CURLOPT_POST, true);  
-//curl_setopt($curl,CURLOPT_POSTFIELDS, 'user_id='.);  
-curl_setopt($curl3,CURLOPT_USERPWD,SWBUSERNAME.":".SWBPASSWORD); 
-$data3 = curl_exec($curl3);     
-curl_error($curl3); 
-curl_close($curl3); 
-echo $data3;
-print_r(simplexml_load_string($data3));*/
-}
 }
 
 
