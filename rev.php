e0e4ea366d8ad6eda3b8821325d459121098a1c6
chenxiaoqino@gmail.com <chenxiaoqino@gmail.com@15126fc1-45a9-b170-bbf4-7e2b6ebb903b>
2011-03-20 09:31:36 +0000

    moving related file from current server.

---
+++ rev.php
@@ -0,0 +1,24 @@
+<?php
+
+require("config.php");
+require("weibo.class.php");
+$w=new weibo(SWBAPPKEY);
+$w->setUser(SWBUSERNAME,SWBPASSWORD);
+
+date_default_timezone_set('Asia/Chongqing');
+$h=date('H');
+echo $h.' <br>';
+if($h<7||$h>22)//gmt+8 23:~6:
+{echo ' sleeping~';
+}
+else{
+$us=file("udl.txt");
+$u='Still remember?'.' [ '.implode($us).'] '.SWBHASHTAG;
+echo $u;
+
+fclose(fopen('udl.txt','w'));
+
+$r=$w->update($u);
+print_r($r);
+}
+?>
