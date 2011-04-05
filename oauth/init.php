241bd41770e7e88864be6774d91ce2d6f7705a50
chenxiaoqino@gmail.com <chenxiaoqino@gmail.com@15126fc1-45a9-b170-bbf4-7e2b6ebb903b>
2011-04-05 04:28:57 +0000

    oauth initialize file

---
+++ oauth/init.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once( 'config.php' );
+include_once( 'weibooauth.php' );
+
+
+
+$o = new WeiboOAuth( SWBAPPKEY, SWBAPPSECRET );
+
+$keys = $o->getRequestToken();
+
+$callback = 'http://wordcard.tk/wbdemo/cbk.php';
+echo 'current callback url: '.$callback.' ; <br>';
+$aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , $callback );
+
+//$_SESSION['keys'] = $keys;
+//print_r($keys);
+$ot=$keys['oauth_token'];
+$ots=$keys['oauth_token_secret'];
+//echo $ot.'-'.$ots;
+$fh=fopen('token.php','w+');
+fwrite($fh,"<?\n");
+fwrite($fh,'$ot='."\"".$ot."\"".';');
+fwrite($fh,'$ots='."\"".$ots."\"".';');
+fwrite($fh,"\n?>\n");
+fclose($fh);
+
+
+?>
+<a href="<?=$aurl?>">Use Oauth to login</a>
