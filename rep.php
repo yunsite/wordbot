ed83e27660a546baa3fa460c5b059600af3ea7ce
chenxiaoqino@gmail.com <chenxiaoqino@gmail.com@15126fc1-45a9-b170-bbf4-7e2b6ebb903b>
2011-04-22 13:22:47 +0000

    Add “查” support

--- rep.php
+++ rep.php
@@ -2,16 +2,20 @@
 require("prepare.php");
 
 $tmf=file('rept.txt');
-$tm=$tmf[0];
+$tm=$tmf[0]; 
 $tm+=RINTERVAL;
 $tn=time();
 if($tn<$tm)
 {
+header("HTTP/1.0 403 Forbidden");
 echo 'time not enough '."\n";
 print($tn.'<'.$tm);
 }
 else
 {
+
+$cha=chr(0xE6).chr(0x9F).chr(0xA5);
+
 $ft=fopen('rept.txt','w');
 fwrite($ft,$tn);
 fclose($ft);
@@ -37,18 +41,16 @@ if($id>$mi){
 $text =strtolower($data['text']);
 if($id>$mmi){$mmi=$id;}
 
-$b= "lookup";
+$drep=0;
+
+$b="lookup";
 $c=explode($b,$text); 
 if(count($c)> 1){
-//print('@'.$name." :".$text."&   ");
 print($id);
-//echo  $c[1];
-
 $xml=simplexml_load_file('http://dict.cn/ws.php?utf8=true&q='.$c[1]);
 if($xml->key=='')
 {
 $u='Sorry, not recognized. ';
-
 $sg=$xml->sugg;
 $ct=count($sg);
 if($ct>0){
@@ -57,19 +59,50 @@ $sgt='Spelling suggestion:';
 for($i=0;$i<$ct;$i+=1)
 $sgt=$sgt.' '.$sg[$i];
 print $sgt;}
-else{$sgt='No spelling suggestionavailable.';}
+else{$sgt='';}
 $u=$u.$sgt;
 }
 else
 {
 $u=$xml->key.' ['.$xml->pron.'] '.$xml->def;
 }
-/*$un=$data['user'];
-$na=$un['name'];
-$u='@'.$na.' '.$u;*/
 echo 'ud:'.$u;
 print_r($w->send_comment($id,$u,0));
+$drep=1;
 }
+
+
+
+$b2=$cha;
+$c=explode($b2,$text); 
+if(count($c)> 1&&$drep==0){
+print($id);
+$xml=simplexml_load_file('http://dict.cn/ws.php?utf8=true&q='.$c[1]);
+if($xml->key=='')
+{
+$u='Sorry, not recognized. ';
+$sg=$xml->sugg;
+$ct=count($sg);
+if($ct>0){
+//echo print_r($sg);
+$sgt='Spelling suggestion:';
+for($i=0;$i<$ct;$i+=1)
+$sgt=$sgt.' '.$sg[$i];
+print $sgt;}
+else{$sgt='';}
+$u=$u.$sgt;
+}
+else
+{
+$u=$xml->key.' ['.$xml->pron.'] '.$xml->def;
+}
+echo 'ud:'.$u;
+print_r($w->send_comment($id,$u,0));
+$drep=1;
+}
+
+
+
 
 }
 }
