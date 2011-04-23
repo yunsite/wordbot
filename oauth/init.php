4d3ba94ba71e538ed11bb519b1efb4d2b673c9e0
chenxiaoqino <chenxiaoqino@gmail.com>
2011-04-23 06:53:39 +0000

    Change to single-file oauth initialize

--- oauth/init.php
+++ oauth/init.php
@@ -4,27 +4,45 @@ include_once( 'config.php' );
 include_once( 'weibooauth.php' );
 
 
+if($_GET['iscallback']==1)
+{
+$keys=unserialize(file_get_contents("token"));
 
+$o = new WeiboOAuth( SWBAPPKEY, SWBAPPSECRET , $keys['oauth_token'],$keys['oauth_token_secret'] );
+
+$last_key = $o->getAccessToken(  $_REQUEST['oauth_verifier'] ) ;
+
+$lkot=$last_key['oauth_token'];
+$lkots=$last_key['oauth_token_secret'];
+
+$fh=fopen('token.php','w');
+fwrite($fh,"<?\n");
+fwrite($fh,'define("SWBTOKEN","'.$lkot.'");'."\n");
+fwrite($fh,'define("SWBTOKENSECRET","'.$lkots.'");'."\n");
+fwrite($fh,"\n?>");
+fclose($fh);
+
+
+echo 'token.php generated.';
+}
+else
+{
 $o = new WeiboOAuth( SWBAPPKEY, SWBAPPSECRET );
 
 $keys = $o->getRequestToken();
 
-  $callback = 'http://yourdimain/oauth/cbk.php';// Please Modify this
+$callback = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?iscallback=1';
 echo 'current callback url: '.$callback.' ; <br>';
 $aurl = $o->getAuthorizeURL( $keys['oauth_token'] ,false , $callback );
 
 //$_SESSION['keys'] = $keys;
 //print_r($keys);
-$ot=$keys['oauth_token'];
-$ots=$keys['oauth_token_secret'];
-//echo $ot.'-'.$ots;
-$fh=fopen('token.php','w+');
-fwrite($fh,"<?\n");
-fwrite($fh,'$ot='."\"".$ot."\"".';');
-fwrite($fh,'$ots='."\"".$ots."\"".';');
-fwrite($fh,"\n?>\n");
+
+$fh=fopen('token','w+');
+fwrite($fh,serialize($keys));
 fclose($fh);
 
+echo '<a href="'.$aurl.'">Use Oauth to login</a>';
+}
 
 ?>
-<a href="<?=$aurl?>">Use Oauth to login</a>
